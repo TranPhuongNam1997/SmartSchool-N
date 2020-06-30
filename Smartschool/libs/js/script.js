60605afebc9f5c8b5b440b02197b7f4749c2f409
TranPhuongNam <tranphuongnam17041997@gmail.com>
2020-06-30 17:52:16 +0700

    3006update

--- Smartschool/libs/js/script.js
+++ Smartschool/libs/js/script.js
@@ -247,6 +247,37 @@ $(document).ready(function () {
         init();
 
     })();
+
+    // Click event of the showPassword button
+    $('.show-pass').on('click', function(){
+
+        // Get the password field
+        var passwordField = $('.password-ss');
+
+        // Get the current type of the password field will be password or text
+        var passwordFieldType = passwordField.attr('type');
+
+        // Check to see if the type is a password field
+        if(passwordFieldType == 'password')
+        {
+            // Change the password field to text
+            passwordField.attr('type', 'text');
+
+            // Change the Text on the show password button to Hide
+            $(this).val('Hide');
+
+            $('.show-pass .img-show-pass').hide();
+            $('.show-pass .img-hide-pass').show();
+        } else {
+            // If the password field type is not a password field then set it to password
+            passwordField.attr('type', 'password');
+
+            // Change the value of the show password button to Show
+            $(this).val('Show');
+            $('.show-pass img.img-show-pass').show();
+            $('.show-pass img.img-hide-pass').hide();
+        }
+    });
 });
 //effect button
 $(function($) {
