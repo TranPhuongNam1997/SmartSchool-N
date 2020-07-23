a29935f79642d2ddcb534c953fde5c89bdb73baf
TranPhuongNam <tranphuongnam17041997@gmail.com>
2020-07-23 18:23:14 +0700

    update

--- Smartschool/libs/js/script.js
+++ Smartschool/libs/js/script.js
@@ -1,78 +1,78 @@
 // $( document ).ready(function() {
 
-    // $('.datepicker-msb-none').focus(function () {
-    //     var getwidth = $('.data-filed .datepicker-msb-none').width();
-    //     $('.xdsoft_datetimepicker.xdsoft_noselect.xdsoft_').css({"width": getwidth + 38});
-    // });
+// $('.datepicker-msb-none').focus(function () {
+//     var getwidth = $('.data-filed .datepicker-msb-none').width();
+//     $('.xdsoft_datetimepicker.xdsoft_noselect.xdsoft_').css({"width": getwidth + 38});
+// });
 
-    // $('.show-child-kl').hide();
-    //
-    // $('.parent_sysbol').click(function () {
-    //     $(this).next('.show-child-kl').toggle();
-    //     $(this).closest('tr.has-group').find('.show-child-kl:nth-child(n+4)').show();
-    // });
+// $('.show-child-kl').hide();
+//
+// $('.parent_sysbol').click(function () {
+//     $(this).next('.show-child-kl').toggle();
+//     $(this).closest('tr.has-group').find('.show-child-kl:nth-child(n+4)').show();
+// });
 
-    //select
-    // $('select').click(function () {
-    //     $(this).toggleClass('select-icon-up');
-    // });
-    // $('select').focusout(function () {
-    //     $(this).removeClass('select-icon-up')
-    // });
-    //
-    // //menubar
-    // var overlay = $("<div class='overlay'></div>")
-    // $("body").prepend(overlay);
-    //
-    // $(".view-menu-mb").click(function () {
-    //     $(this).toggleClass("change-icon-mb")
-    //     overlay.toggle();
-    //     $(".navigation").toggleClass("open-mb");
-    // });
-    // overlay.click(function () {
-    //    $(".view-menu-mb").trigger('click');
-    // });
-    //
-    // //close file name-uploadfile
-    // $(".close-file").click(function () {
-    //     $(this).parent(".file-return").remove();
-    // });
-    //
-    // login_pp();
+//select
+// $('select').click(function () {
+//     $(this).toggleClass('select-icon-up');
+// });
+// $('select').focusout(function () {
+//     $(this).removeClass('select-icon-up')
+// });
+//
+// //menubar
+// var overlay = $("<div class='overlay'></div>")
+// $("body").prepend(overlay);
+//
+// $(".view-menu-mb").click(function () {
+//     $(this).toggleClass("change-icon-mb")
+//     overlay.toggle();
+//     $(".navigation").toggleClass("open-mb");
+// });
+// overlay.click(function () {
+//    $(".view-menu-mb").trigger('click');
+// });
+//
+// //close file name-uploadfile
+// $(".close-file").click(function () {
+//     $(this).parent(".file-return").remove();
+// });
+//
+// login_pp();
 
-    // $('.search-select2').select2();
+// $('.search-select2').select2();
 
-    // js show hide table
-    // $(".zone-extend").hide();
-    // $('.zone-narrow').click(function () {
-    //     $(this).parent().find('.zone-extend').toggle();
-    //
-    // });
+// js show hide table
+// $(".zone-extend").hide();
+// $('.zone-narrow').click(function () {
+//     $(this).parent().find('.zone-extend').toggle();
+//
+// });
 
-    // var count = $('.table-wrap .table-block table tbody tr.has-group td > div.ex-cls').length;
-    // if(count > 3){
-    //     $('.ex-cls:nth-child(n+4)').hide();
-    //     $('.btn-collapse').hide();
-    // }
-    // else{
-    //     $('.btn-extend-collapse').hide();
-    // }
-    //
-    // $('.btn-extend').click(function () {
-    //     $(this).hide();
-    //     $(this).next().show();
-    //     $(this).closest('tr.has-group').find('.ex-cls:nth-child(n+4)').show();
-    //
-    //     $(this).closest('tr.has-group').find('.ex-cls:nth-child(3)').css('border-bottom','1px solid #eaeaea');
-    //     $(this).parent('.btn-extend-collapse').prev().css('border-bottom','none');
-    // });
-    // $('.btn-collapse').click(function () {
-    //     $(this).hide();
-    //     $(this).prev().show();
-    //     $(this).closest('tr.has-group').find('.ex-cls:nth-child(n+4)').hide();
-    //     $(this).parent('.btn-extend-collapse').prev('.ex-cls:nth-child(3)').css('border-bottom','none')
-    //     $(this).closest('tr.has-group').find('.ex-cls:nth-child(3)').css('border-bottom','none');
-    // })
+// var count = $('.table-wrap .table-block table tbody tr.has-group td > div.ex-cls').length;
+// if(count > 3){
+//     $('.ex-cls:nth-child(n+4)').hide();
+//     $('.btn-collapse').hide();
+// }
+// else{
+//     $('.btn-extend-collapse').hide();
+// }
+//
+// $('.btn-extend').click(function () {
+//     $(this).hide();
+//     $(this).next().show();
+//     $(this).closest('tr.has-group').find('.ex-cls:nth-child(n+4)').show();
+//
+//     $(this).closest('tr.has-group').find('.ex-cls:nth-child(3)').css('border-bottom','1px solid #eaeaea');
+//     $(this).parent('.btn-extend-collapse').prev().css('border-bottom','none');
+// });
+// $('.btn-collapse').click(function () {
+//     $(this).hide();
+//     $(this).prev().show();
+//     $(this).closest('tr.has-group').find('.ex-cls:nth-child(n+4)').hide();
+//     $(this).parent('.btn-extend-collapse').prev('.ex-cls:nth-child(3)').css('border-bottom','none')
+//     $(this).closest('tr.has-group').find('.ex-cls:nth-child(3)').css('border-bottom','none');
+// })
 
 // });
 
@@ -109,9 +109,9 @@
 //     }
 // }
 $(document).ready(function () {
-   $('.wrap-search i').click(function () {
-       $('.block-seacrh').toggle();
-   });
+    $('.wrap-search i').click(function () {
+        $('.block-seacrh').toggle();
+    });
     // $(window).scroll(function () {
     //     if ($(window).scrollTop() >= 300) {
     //         $('.header-gd').addClass('fixed');
@@ -150,6 +150,15 @@ $(document).ready(function () {
         $('.overlay').toggle();
         $('.navigation').toggleClass('show');
     });
+
+    //js select
+
+    $('.field-select').click(function () {
+       $(this).toggleClass('active');
+    });
+    $('.field-select').focusout(function () {
+        $(this).removeClass('active');
+    })
 
 
 
@@ -305,8 +314,38 @@ $(document).ready(function () {
             $('.show-pass img.img-hide-pass').hide();
         }
     });
+    $('.show-pass2').on('click', function(){
 
-    videoanimation();
+        // Get the password field
+        var passwordField = $('#re_password');
+
+        // Get the current type of the password field will be password or text
+        var passwordFieldType = passwordField.attr('type');
+
+        // Check to see if the type is a password field
+        if(passwordFieldType == 'password')
+        {
+            // Change the password field to text
+            passwordField.attr('type', 'text');
+
+            // Change the Text on the show password button to Hide
+            $(this).val('Hide');
+
+            $('.show-pass2 .img-show-pass').hide();
+            $('.show-pass2 .img-hide-pass').show();
+        } else {
+            // If the password field type is not a password field then set it to password
+            passwordField.attr('type', 'password');
+
+            // Change the value of the show password button to Show
+            $(this).val('Show');
+            $('.show-pass2 img.img-show-pass').show();
+            $('.show-pass2 img.img-hide-pass').hide();
+        }
+    });
+
+
+    //videoanimation();
 
 });
 //effect button
